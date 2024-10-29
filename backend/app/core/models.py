6695cf6242411144064c445e634624178413a12e
David <david.miklos@siemens.com>
2024-10-29 12:18:55 +0200

    add db and migrations

---
+++ backend/app/core/models.py
@@ -0,0 +1,17 @@
+import uuid
+
+from pydantic import EmailStr
+from sqlmodel import Field, SQLModel
+
+
+# Shared properties
+class UserBase(SQLModel):
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
+    full_name: str | None = Field(default=None, max_length=255)
+    foo: bool = False
+
+
+# Database model, database table inferred from class name
+class User(UserBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    hashed_password: str
